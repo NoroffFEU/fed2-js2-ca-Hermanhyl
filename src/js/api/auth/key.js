fdf45d96674626ea8f7100805f652c0db8cc3eb6
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-20 13:49:11 +0200

    created Register, login and get key functions

--- src/js/api/auth/key.js
+++ src/js/api/auth/key.js
@@ -1,1 +1,32 @@
-export async function getKey(name) {}
+import {API_AUTH_KEY, } from "../constants"
+
+ export const getAuthToken = () => {
+     const accessToken = localStorage.getItem('token');
+     return accessToken;
+ };
+
+ const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQnJvciIsImVtYWlsIjoiYnJvckBzdHVkLm5vcm9mZi5ubyIsImlhdCI6MTcyNjU2OTgwMH0.mqi8G2Qazu-ywTqJcdToHuFz98i6v14LPcKd0s5BFyw"
+
+export async function getKey() {
+
+    const response = await fetch(API_AUTH_KEY, {
+        method: `POST`,
+        headers: { 
+            "Content-Type": "application/json" ,
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            name: "TestKey",
+        })
+    });
+
+    if (response.ok) {
+        return await response.json();
+    }
+
+    console.error(await response.json());
+    throw new Error("Could not register for key!")
+}
+
+getKey().then(console.log)
+
--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,46 @@
-export async function login({ email, password }) {}
+import {API_AUTH_LOGIN } from "../constants";
+import { headers } from "../headers"
+
+const loginButton = document.getElementById("login-button")
+loginButton.addEventListener("click", handleLoginClick)
+
+function handleLoginClick() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email && password) {
+        login({ email, password });
+    } else {
+        alert("Please enter both email and password.");
+    }
+}
+
+export async function login({ email, password }) {
+    const body = {
+        email: email,
+        password: password
+    }
+    try {
+        const response = await fetch(API_AUTH_LOGIN, {
+            method: "POST",
+            headers: headers(),
+            body: JSON.stringify(body),
+        });
+        
+        if (response.ok) {
+            const data = await response.json();
+            const accessToken = data.data.accessToken;
+            localStorage.setItem("token", accessToken);
+            window.location.href = "/"
+            alert(`Successfully logged in`);
+            
+
+
+        } 
+    } catch (error) {
+        console.error("Error during login:", error);
+        alert(`An error occurred: ${error.message}`);
+    }
+}
+
+
