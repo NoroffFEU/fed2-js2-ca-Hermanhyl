5565a1bef3169a818822499548fc82646a762b10
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-24 10:12:48 +0200

    Create post function

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,34 @@
-export async function createPost({ title, body, tags, media }) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers } from "../headers";
+
+     // const accessToken = getAuthToken();
+    //const createButton = document.getElementById("create-button");
+
+export async function createPost({ title, body, tags, image, alt }) {
+    
+        const requestBody = {
+            title: title,
+            body: body,
+            tags: [tags],
+            url: image,
+            alt: alt, 
+        };
+        console.log("request-body", requestBody);
+        
+        try {
+            const response = await fetch(API_SOCIAL_POSTS, {
+                method: "POST",
+                headers: headers(),
+                body: JSON.stringify(requestBody),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+            console.log("Post created successfully:", data);
+            return data
+            }
+            
+        } catch (error) {
+            console.error("Error creating post:", error);
+        }
+}
