cc09f9382925ce18e87e2307cba43437b55cd639
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-24 10:15:25 +0200

    added on create post function

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,18 @@
-export async function onCreatePost(event) {}
+import { createPost } from "../../api/post/create";
+
+export async function onCreatePost(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const createData = {
+        title: formData.get("title"), 
+        body: formData.get("body"), 
+        tags: formData.get("tags"), 
+        url: formData.get("image"),
+        alt: formData.get("alt")
+        
+    }
+
+    createPost(createData)
+}
