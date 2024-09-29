a13f6b5d56198fb7621dc056d145399af7f51951
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 13:18:10 +0200

    comented in potential code for later use

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,3 +1,55 @@
-import { authGuard } from "../../utilities/authGuard";
+// import { readPostsByUser } from "../../api/post/read";
+// import { setLogoutListener } from "../../ui/global/logout";
 
-authGuard();
+//     export const displayUserPost = (posts) => {
+//         const postContainer = document.getElementById("postUserContainer");
+    
+//         if (!postContainer) {
+//             console.error("No container with the id 'postUserContainer' found.");
+//             return;
+//         }
+    
+//         posts.forEach((profilePost) => {
+//             const container = document.createElement("div");
+//             container.className = "postContainer";
+    
+//             const title = document.createElement("h2");
+//             title.innerText = profilePost.title;
+    
+//             const userName = document.createElement("p");
+//             userName.innerText = `Posted by: ${profilePost.author.name}`;
+    
+//             const postDate = document.createElement("p");
+//             postDate.innerText = `Posted on: ${new Date(profilePost.created).toLocaleDateString()}`;
+    
+//             const text = document.createElement("p");
+//             text.innerText = profilePost.body;
+
+//             const tags = document.createElement("p")
+//             text.innerText = profilePost.tags;
+    
+//             const image = document.createElement("img");
+    
+//             if (profilePost.media && profilePost.media.url) {
+//                 image.src = profilePost.media.url;
+//                 image.alt = profilePost.media.url;
+//                 image.className = "postImage";
+//                 container.append(image);
+//             }
+    
+//             container.append(title, text, tags, userName, postDate);
+//             postContainer.append(container);
+//         });
+//     };
+        
+
+//     async function runPage() {
+//         const post = await readPostsByUser()
+//         displayUserPost(post)
+//     }
+    
+//     runPage()
+
+//     setLogoutListener()
+
+  
