f5f8db428f40bbaa98d63c41320b3bc3f2deb993
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 12:38:42 +0200

    js doc description of loadPost function

--- src/js/router/views/postEdit.js
+++ src/js/router/views/postEdit.js
@@ -6,6 +6,18 @@ import { authGuard } from "../../utilities/authGuard";
 
 const postId = JSON.parse(localStorage.getItem("postId"));
 
+/**
+ * Loads a post and populates the edit form with its details.
+ * 
+ * This asynchronous function fetches a post using `readPost` and populates the form fields for editing.
+ * It sets the title, body, tags, media URL, and alt text of the post in the corresponding form elements.
+ * If the post is not found or an error occurs during the fetching, an appropriate error message is logged.
+ * 
+ * @async
+ * @function loadPost
+ * @returns {Promise<void>} Resolves when the post details have been loaded into the form.
+ * @throws {Error} If an error occurs while fetching the post.
+ */
 
 async function loadPost() {
 
