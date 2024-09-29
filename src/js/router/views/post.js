37718ec3acf103b768370374def6e400eebcdb9f
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 12:37:51 +0200

    removed unnecessary import and alert. also added js doc description for displayPost function

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,10 +1,25 @@
 import { readPost } from "../../api/post/read";
-import { setLogoutListener } from "../../ui/global/logout";
 import { onDeletePost } from "../../ui/post/delete";
 
+/**
+ * Displays the details of a single post in the HTML container with the ID "postContainer".
+ * 
+ * This function creates and appends elements for the post, including the title, author, date, body,
+ * media (if available), and buttons for deleting and editing the post.
+ * The delete button triggers the `onDeletePost` function, while the edit button redirects to the edit page.
+ * 
+ * @function displayPost
+ * @param {Object} post - The post object to display.
+ * @param {string} post.title - The title of the post.
+ * @param {Object} post.author - The author of the post.
+ * @param {string} post.author.name - The name of the author.
+ * @param {string} post.body - The content of the post.
+ * @param {string} post.created - The creation date of the post in ISO format.
+ * @param {Object} [post.media] - Optional media object associated with the post.
+ * @param {string} [post.media.url] - The URL of the post's media.
+ * @returns {void}
+ */
 
-
-alert("Single Post Page");
 export const displayPost = (post) => {
     const postContainer = document.getElementById("postContainer");
 
