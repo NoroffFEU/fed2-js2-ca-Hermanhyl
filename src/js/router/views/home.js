b90fd398c1b9cc91c17aa1617579d4b347b6d74a
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 12:35:58 +0200

    Js doc description of runPage function

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -3,6 +3,16 @@ import { readPosts } from "../../api/post/read";
 import { setLogoutListener } from "../../ui/global/logout";
 import { authGuard } from "../../utilities/authGuard";
 
+/**
+ * Retrieves and displays a list of posts on the page.
+ * 
+ * This asynchronous function calls `readPosts` to fetch the posts and then
+ * passes the retrieved posts to the `displayPosts` function to render them in the UI.
+ * 
+ * @async
+ * @function runPage
+ * @returns {Promise<void>} Resolves when the posts have been fetched and displayed.
+ */
 
 async function runPage() {
     const posts = await readPosts()
