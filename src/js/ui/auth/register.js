378166cf7688f210d74332a40728f48e91c4f519
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 13:09:48 +0200

    added js doc description for onRegister function

--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,5 +1,19 @@
 import { register } from "../../api/auth/register";
 
+/**
+ * Handles the registration form submission event.
+ * 
+ * This asynchronous function prevents the default form submission behavior,
+ * retrieves the user's name, email, and password from the form data, and calls the
+ * `register` function with the extracted registration details.
+ * 
+ * @async
+ * @function onRegister
+ * @param {Event} event - The event object representing the form submission event.
+ * @returns {Promise<void>} Resolves when the registration process is initiated.
+ * @throws {Error} If an error occurs during the registration process.
+ */
+
 export async function onRegister(event) {
     event.preventDefault();
     const formData = new FormData(event.target)
