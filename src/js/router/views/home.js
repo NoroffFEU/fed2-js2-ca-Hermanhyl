296cd11e6ef99595761d47bcda7635ca1e0f8be9
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 12:29:38 +0200

    added runPage function and imported needed functions to call

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,3 +1,16 @@
+import { displayPosts } from "../../api/post/posts-Structure";
+import { readPosts } from "../../api/post/read";
+import { setLogoutListener } from "../../ui/global/logout";
 import { authGuard } from "../../utilities/authGuard";
+
+
+async function runPage() {
+    const posts = await readPosts()
+    displayPosts(posts)
+}
+
+runPage();
+
+setLogoutListener();
 
 authGuard();
