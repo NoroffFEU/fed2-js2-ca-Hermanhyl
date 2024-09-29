10c0bacc80c2e1b6ac638b93f0a82d6eab8bec45
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 12:05:21 +0200

    added fetch call for updating posts

--- src/js/api/post/update.js
+++ src/js/api/post/update.js
@@ -1,1 +1,26 @@
-export async function updatePost(id, { title, body, tags, media }) {}
+import { API_SOCIAL_POSTS } from "../constants"
+import { headers } from "../headers"
+
+export async function updatePost(id, { title, body, tags, media }) {
+    id = new URLSearchParams(window.location.search).get('id');
+
+    try {
+        const response = await fetch (`${API_SOCIAL_POSTS}/${id}`, {
+            method: 'PUT',
+            headers: headers(),
+            body: JSON.stringify({title, body, tags, media})
+        });
+
+        if (!response.ok) {
+            alert('Failed to update post')
+
+        } else {
+            alert('Post successfully updated')
+            //window.location.href = /post/?id=${id}
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        console.error('Failed to update the post:', error);
+    }
+}
