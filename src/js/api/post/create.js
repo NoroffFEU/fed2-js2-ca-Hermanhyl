962a57db52c4ecb66ec067ce9f21a21b0a88fc69
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 12:09:56 +0200

    this is the js docs for the create. previus push was changing of form data setup

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,6 +1,22 @@
 import { API_SOCIAL_POSTS } from "../constants";
 import { headers } from "../headers";
 
+/**
+ * Creates a new post by sending the post details to the server.
+ * 
+ * This function sends a POST request to the social posts API with the post's title, body, tags, and media.
+ * If the post is created successfully, the response data is returned, otherwise, an error is logged.
+ *
+ * @async
+ * @function createPost
+ * @param {Object} params - The post details.
+ * @param {string} params.title - The title of the post.
+ * @param {string} params.body - The content of the post.
+ * @param {Array<string>} [params.tags] - Optional tags for the post.
+ * @param {Array<string>} [params.media] - Optional media links for the post.
+ * @returns {Promise<Object|undefined>} The data of the created post, or undefined if an error occurs.
+ * @throws {Error} If the post creation fails.
+ */
 
 export async function createPost({ title, body, tags, media }) {
     
