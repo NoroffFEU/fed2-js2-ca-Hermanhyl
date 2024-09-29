6b7d011307cb47d922c5f30c079a771b9debebcc
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 12:10:30 +0200

    added delete function

--- src/js/ui/post/delete.js
+++ src/js/ui/post/delete.js
@@ -1,1 +1,19 @@
-export async function onDeletePost(event) {}
+import { deletePost } from "../../api/post/delete";
+
+export async function onDeletePost(id) {
+    
+    const confirmDelete = confirm('Are you sure you want to delete this post?');
+
+    if (confirmDelete) {
+        const success = await deletePost(id);  // Call the deletePost function
+
+        if (success) {
+            alert('Post successfully deleted!');
+        } else {
+            alert('Failed to delete the post.');
+        }
+    } else {
+        console.log('User canceled the delete action.');
+    }
+    
+}
