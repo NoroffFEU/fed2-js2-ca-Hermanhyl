4cc5ebf54f91bb072e2340e5b7484c7cdb6fa153
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 13:08:27 +0200

    added login js doc documentation for the onLoginfunction

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,5 +1,19 @@
 import { login } from "../../api/auth/login";
 
+/**
+ * Handles the login form submission event.
+ * 
+ * This asynchronous function prevents the default form submission behavior,
+ * retrieves the email and password from the form data, and calls the `login` function
+ * with the extracted login credentials.
+ * 
+ * @async
+ * @function onLogin
+ * @param {Event} event - The event object representing the form submission event.
+ * @returns {Promise<void>} Resolves when the login process is initiated.
+ * @throws {Error} If an error occurs during the login process.
+ */
+
 export async function onLogin(event) {
     event.preventDefault();
 
