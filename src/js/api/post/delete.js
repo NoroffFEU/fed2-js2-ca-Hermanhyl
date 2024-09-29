487d627508409deef221e88250dcd88b3cd7691d
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 12:12:11 +0200

    Js doc coment added to the deletePost function

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,6 +1,17 @@
 import { API_SOCIAL_POSTS } from "../constants";
 import { headers } from "../headers"
 
+/**
+ * Deletes a post based on the post ID from the URL query parameters.
+ * 
+ * This function retrieves the post ID from the current URL's query string and sends a DELETE request to the server.
+ * If the deletion is successful, a confirmation message is logged. Otherwise, an error message is displayed.
+ * 
+ * @async
+ * @function deletePost
+ * @returns {Promise<boolean>} Returns `true` if the post is successfully deleted, or `false` if an error occurs.
+ * @throws {Error} If the deletion request fails.
+ */
 
 export async function deletePost() {
     const id = new URLSearchParams(window.location.search).get('id');
