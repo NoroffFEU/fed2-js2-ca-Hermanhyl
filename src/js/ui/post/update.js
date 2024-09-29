f3580f9f53c793968a49590947bb467607895c5f
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 13:16:28 +0200

    added onUpdate function and js doc description

--- src/js/ui/post/update.js
+++ src/js/ui/post/update.js
@@ -1,1 +1,37 @@
-export async function onUpdatePost(event) {}
+import { updatePost } from "../../api/post/update";
+
+/**
+ * Handles the update of an existing post from a form submission.
+ * 
+ * This asynchronous function prevents the default form submission behavior,
+ * retrieves the title, body, tags, and media information from the form data,
+ * and calls the `updatePost` function with the specified post ID and the
+ * prepared update data. The tags are processed into an array, and the media
+ * object is structured to include a URL and alt text.
+ * 
+ * @async
+ * @function onUpdatePost
+ * @param {Event} event - The event object representing the form submission event.
+ * @param {string} postId - The unique identifier of the post to be updated.
+ * @returns {Promise<void>} Resolves when the post update process is initiated.
+ * @throws {Error} If an error occurs during the post update process.
+ */
+
+export async function onUpdatePost(event, postId) {
+    event.preventDefault(); 
+    
+    const form = new FormData(event.target);
+
+    const editInfo = {
+        title: form.get('title'),
+        body: form.get('body'),
+        tags: form.get('tags') ? form.get('tags').split(', ').map(tag => tag.trim()) : [],
+        media: {
+            url: form.get('image'),
+            alt: form.get('alt') 
+        }
+    };
+
+    updatePost(postId, editInfo)
+
+}
