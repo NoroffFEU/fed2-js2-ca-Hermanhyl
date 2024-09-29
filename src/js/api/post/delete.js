4a1f9f8a09070d8594d284250685c3b260ed9918
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-09-29 12:03:21 +0200

    delete function

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,1 +1,24 @@
-export async function deletePost(id) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers } from "../headers"
+
+
+export async function deletePost() {
+    const id = new URLSearchParams(window.location.search).get('id');
+    try {
+        const response = await fetch(`${API_SOCIAL_POSTS}/${id}`, {
+            method: 'DELETE',
+            headers: headers()
+        });
+
+        if (response.ok) {
+            console.log('Post successfully deleted');
+        } else {
+            const errorMessage = await response.text(); 
+            console.error('Failed to delete post:', errorMessage);
+            return false;
+        }
+    } catch (error) {
+        console.error('Error deleting post:', error);
+        return false;
+    }
+}
